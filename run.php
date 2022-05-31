c837e2c52fec4ffbef9ac6627ca7830864b3c869
rudy2306 <106556475+rudy2306@users.noreply.github.com>
2022-05-31 10:39:24 +0700

    Update run.php

--- run.php
+++ run.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/userAgent.php';
 require __DIR__ . '/smshub.php';
 
 //APIKEY SMSHUB
-$key = '';
+$key = '127870U71e26cf7c26b8e316bcbf0fb923f474d';
 
 if($key == null) {
     die('Masukan apikey di line 9 goblog!!');
